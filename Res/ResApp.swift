93f4027ef1ed12a15eb0cca158d159f1ed9ab9c8
restraint444 <restraintrocks@gmail.com>
2025-07-31 09:36:47 +1000

    Initial Commit

---
+++ Res/ResApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResApp.swift
+//  Res
+//
+//  Created by Ethan Tan on 31/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct ResApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
